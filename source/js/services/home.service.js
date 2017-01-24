aefef1da624def34456c81af628df240b1e0a620
Michal <michal@MacBook-Michal.local>
2017-01-24 15:46:09 +0100

    post method testing

--- source/js/services/home.service.js
+++ source/js/services/home.service.js
@@ -15,6 +15,11 @@ class HomeService {
             .then(this.handleSuccess, this.handleError);
     }
 
+    savePost(title, body){
+        return this.$http.post('http://jsonplaceholder.typicode.com/posts', {title: title, body: body})
+            .then(this.handleSuccess, this.handleError);
+    }
+
     handleSuccess(res) {
         console.log('Success!');
         return res.data;
